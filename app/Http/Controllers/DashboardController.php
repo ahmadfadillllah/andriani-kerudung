b734f9d6bdb9334832ed7efef4cb4ceadf5e3c4c
ahmadfadillllah <ahmadfadillah502@gmail.com>
2023-11-07 13:04:15 +0800

    first

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,13 +2,23 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Order;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class DashboardController extends Controller
 {
     //
     public function index()
     {
-        return view('dashboard.index');
+        $now = Carbon::now()->format('d');
+        $bulan = Carbon::now()->format('m');
+
+        $keranjang = DB::table('cart')->whereMonth('created_at',$now)->count();
+        $order = DB::table('order')->whereMonth('created_at',$now)->count();
+        $pengguna = DB::table('cart')->whereMonth('created_at',$bulan)->count();
+
+        return view('dashboard.index', compact('keranjang', 'order', 'pengguna'));
     }
 }
